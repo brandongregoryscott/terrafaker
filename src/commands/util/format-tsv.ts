d3205298851cc1044cf0985702e2932c81a82de3
Brandon Scott <contact@brandonscott.me>
2025-12-13 17:14:41 -0500

    feat: initial round of gcp constants + utility command for parsing docs

---
+++ src/commands/util/format-tsv.ts
@@ -0,0 +1,43 @@
+import { Args, Flags } from "@oclif/core";
+import { BaseCommand } from "../../utilities/base-command.js";
+import { stringifySingleLineArray } from "../../utilities/string-utils.js";
+
+const EXAMPLE = `c4d-standard-2	2	7	No	Up to 10	N/A`;
+
+class FormatTsv extends BaseCommand {
+    static hidden = true;
+
+    static description =
+        "Utility command for formatting a TSV (tab-separated value) into an array or object with array values. Primarily used for parsing data from the GCP docs.";
+
+    static flags = {
+        "column-index": Flags.integer({
+            description: "Column index to pull from each line.",
+            default: 0,
+        }),
+    };
+
+    static args = {
+        tsv: Args.string({
+            required: true,
+            description: `Tab-separated value to format into a string array, i.e. '${EXAMPLE}'.
+If the string is multiple lines (which it generally is), the specified column index from each line will be placed in the array.`,
+        }),
+    };
+
+    async run(): Promise<void> {
+        const { args, flags } = await this.parse(FormatTsv);
+        const columnIndex = flags["column-index"];
+        const { tsv } = args;
+
+        const lines = tsv.split("\n");
+        const values = lines.map((line) => splitTsv(line)[columnIndex]);
+
+        this.log(stringifySingleLineArray(values));
+    }
+}
+
+const splitTsv = (tsv: string): string[] =>
+    tsv.split("\t").map((value) => value.trim());
+
+export { FormatTsv };
