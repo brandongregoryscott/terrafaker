87be5f4d6abef5939a644428b8dde39f2a231a30
Brandon Scott <contact@brandonscott.me>
2025-12-13 13:46:23 -0500

    chore: remove oclif eslint config, add typescript-eslint and collation + light rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,74 @@
 import { includeIgnoreFile } from "@eslint/compat";
-import oclif from "eslint-config-oclif";
-import prettier from "eslint-config-prettier";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
+import typescriptEslint from "typescript-eslint";
+import collation from "eslint-plugin-collation";
 
 const gitignorePath = path.resolve(
     path.dirname(fileURLToPath(import.meta.url)),
     ".gitignore"
 );
 
-export default [includeIgnoreFile(gitignorePath), ...oclif, prettier];
+const config = defineConfig([includeIgnoreFile(gitignorePath)], {
+    languageOptions: {
+        parser: typescriptEslint.parser,
+        parserOptions: {
+            project: "./tsconfig.json",
+        },
+    },
+    files: ["**/*.ts"],
+    ignores: ["**/*.test.ts"],
+    plugins: {
+        "@stylistic": stylistic,
+        "@typescript-eslint": typescriptEslint.plugin,
+        collation,
+    },
+    rules: {
+        curly: "error",
+        "no-var": "error",
+        "@stylistic/padding-line-between-statements": [
+            "error",
+            {
+                blankLine: "always",
+                next: "export",
+                prev: "*",
+            },
+            {
+                blankLine: "never",
+                next: "export",
+                prev: "export",
+            },
+            {
+                blankLine: "always",
+                next: "*",
+                prev: "import",
+            },
+            {
+                blankLine: "always",
+                next: "*",
+                prev: "interface",
+            },
+            {
+                blankLine: "always",
+                next: "*",
+                prev: "type",
+            },
+            {
+                blankLine: "never",
+                next: "import",
+                prev: "import",
+            },
+        ],
+        "@typescript-eslint/consistent-type-definitions": "error",
+        "@typescript-eslint/consistent-type-exports": "error",
+        "@typescript-eslint/consistent-type-imports": "error",
+        "collation/group-exports": "error",
+        "collation/no-default-export": "error",
+        "collation/no-inline-export": "error",
+        "collation/sort-exports": "error",
+    },
+});
+
+export default config;
