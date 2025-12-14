c4dce401378fe793a14bf2c0b8a1630fa8b32338
Brandon Scott <contact@brandonscott.me>
2025-12-14 18:50:21 -0500

    refactor: move provider enum <> class instance method to static ProviderGenerator method

--- src/utilities/generators/file-generator.ts
+++ src/utilities/generators/file-generator.ts
@@ -1,8 +1,5 @@
-import { Providers, type Provider } from "../../enums/providers.js";
-import { AwsGenerator } from "./aws-generator.js";
-import { AzureGenerator } from "./azure-generator.js";
-import { GcpGenerator } from "./gcp-generator.js";
-import type { ProviderGenerator } from "./provider-generator.js";
+import { type Provider } from "../../enums/providers.js";
+import { ProviderGenerator } from "./provider-generator.js";
 
 interface GenerateFileByProviderOptions {
     provider: Provider;
@@ -23,27 +20,13 @@ class FileGenerator {
             directory,
             format,
         } = options;
-        const generator = this.getGeneratorByProvider(provider);
+        const generator = ProviderGenerator.get(provider);
 
         for (let i = 0; i < resourceCount; i++) {
             generator.addRandomResource();
         }
 
         generator.writeToFile({ fileName, directory, format });
-    }
-
-    public static getGeneratorByProvider(
-        provider: Provider
-    ): ProviderGenerator {
-        switch (provider) {
-            case Providers.Azure:
-                return new AzureGenerator();
-            case Providers.GCP:
-                return new GcpGenerator();
-            default:
-            case Providers.AWS:
-                return new AwsGenerator();
-        }
     }
 }
 
