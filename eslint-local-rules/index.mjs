c6cfd23b1d826e73b8094e977b263850b6e7fa8c
Brandon Scott <contact@brandonscott.me>
2026-02-14 11:02:01 -0500

    chore: update node, eslint packages, use collation over local rule (#37)

--- eslint-local-rules/index.mjs
+++ eslint-local-rules/index.mjs
@@ -1,13 +1,9 @@
-import preferPrivateClassMembers from "./prefer-private-class-members.mjs";
-
 /**
  * ESLint plugin exporting custom rules
  * @type {ESLintPlugin}
  */
 const plugin = {
-    rules: {
-        "prefer-private-class-members": preferPrivateClassMembers,
-    },
+    rules: {},
 };
 
 export default plugin;
